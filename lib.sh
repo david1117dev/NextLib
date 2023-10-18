3d975922584a3711a8d4920f8441c7feb0cbc9ab
david1117dev <110995499+david1117dev@users.noreply.github.com>
2023-10-18 12:29:38 +0200

    Add files via upload

--- lib.sh
+++ lib.sh
@@ -19,15 +19,18 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
-
-BLUE='\033[34m'
+BLUE='\033[38;5;32;1m'
 GREEN='\033[1;32m'
 RED='\033[1;31m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 RESET='\e[0m'
-
+if [ "$WATERMARK" != "false" ]; then
+    echo -e "\033[1;32m-------------------------\033[0m"
+    echo -e "\033[1;32m[LIB]\033[0m \033[1;32mPowered by \033[0;32mNextLib\033[0m"
+    echo -e "\033[1;32m-------------------------\033[0m"
+fi
 info() {
     echo -e "${GREEN}[INFO]${NC} ${WHITE}$1${NC}"
 }
@@ -43,6 +46,13 @@ error() {
 fatal() {
     echo -e "${RED}[FATAL]${NC} ${WHITE}$1${NC}"
 }
+runasroot() {
+    if [[ $EUID -ne 0 ]]; then
+        fatal "This script requires root privileges. Please run it as root or with sudo."
+        exit 1
+    fi
+}
+
 check_distribution() {
     supported_distributions=("$@")
     if [[ -f /etc/os-release ]]; then
@@ -85,32 +95,28 @@ mariadb_manage() {
     if [ "$pass" = "random" ]; then
         pass=$(openssl rand -base64 12)
         warn "Generated random password: $pass"
+        export PASS=$pass
     fi
-    export PASS=$pass
     
     case "$action" in
         dbcreate)
             info "Creating database: $name"
-            mariadb -e "CREATE DATABASE IF NOT EXISTS $name;"
-            info "Database $name created."
+            mariadb -e "DROP DATABASE IF EXISTS $name; CREATE DATABASE $name;"
             ;;
 
         dbdelete)
             info "Deleting database: $name"
             mariadb -e "DROP DATABASE IF EXISTS $name;"
-            info "Database $name deleted."
             ;;
 
         usercreate)
             info "Creating user: $name"
-            mariadb -e "CREATE USER IF NOT EXISTS '$name'@'localhost' IDENTIFIED BY '$pass';"
-            info "User $name created."
+            mariadb -e "DROP USER IF EXISTS $name; CREATE USER '$name'@'127.0.0.1' IDENTIFIED BY '$pass';"
             ;;
 
         userdelete)
             info "Deleting user: $name"
             mariadb -e "DROP USER IF EXISTS '$name'@'localhost';"
-            info "User $name deleted."
             ;;
 
         *)
@@ -119,12 +125,60 @@ mariadb_manage() {
             ;;
     esac
 }
+
+question() {
+    local question_text="$1"
+    local response_var="$2"
+    local validate_type="$3"
+    local user_response
+
+    while true; do
+        echo -e "${BLUE}[QUESTION]${NC} ${WHITE}$question_text${NC}"
+        if [ "$validate_type" == "password" ]; then
+            read -s -p "Your answer: " user_response
+        else
+            read -p "Your answer: " user_response
+        fi
+
+        if [ -n "$validate_type" ]; then
+            if [ "$validate_type" == "url" ]; then
+                if [[ $user_response =~ ^(http:\/\/localhost|http:\/\/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|https?:\/\/(localhost|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|[a-zA-Z0-9.-]+\.[a-zA-Z]+))$ ]]; then
+                    echo "Valid URL."
+                    eval "$response_var=\"$user_response\""
+                    break
+                else
+                    echo "Invalid URL. Please try again."
+                fi
+            elif [ "$validate_type" == "email" ]; then
+                if [[ $user_response =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]; then
+                    echo "Valid email."
+                    eval "$response_var=\"$user_response\""
+                    break
+                else
+                    echo "Invalid email. Please try again."
+                fi
+            elif [ "$validate_type" == "password" ]; then
+                echo  # Add a newline after the hidden password input
+                eval "$response_var=\"$user_response\""
+                break
+            else
+                echo "Validation type not supported."
+                break
+            fi
+        else
+            eval "$response_var=\"$user_response\""
+            break
+        fi
+    done
+}
+
 replace() {
     local file="$1"
     local search_string="$2"
     local replace_string="$3"
 
     if [ -f "$file" ]; then
+        # Use sed with double quotes to allow variable substitution
         sed -i "s|$search_string|$replace_string|g" "$file"
         info "Replaced '$search_string' with '$replace_string' in $file."
     else
